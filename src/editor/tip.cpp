SgtBilko76/SuperTux-3D

commit cf8f11904e46f5be6afa647d56827069113508fc

Pass Vectors as (const?) refs where possible

src/editor/tip.cpp
@@ -56,7 +56,7 @@ Tip::~Tip() {
 }
 
 void
-Tip::draw(DrawingContext& context, Vector pos) {
+Tip::draw(DrawingContext& context, Vector& pos) {
   pos.y += 35;
   context.draw_text(Resources::normal_font, header, pos,
                     ALIGN_LEFT, LAYER_GUI-11, ColorScheme::Menu::label_color);
@@ -69,8 +69,9 @@ Tip::draw(DrawingContext& context, Vector pos) {
 }
 
 void
-Tip::draw_up(DrawingContext& context, Vector pos) {
-  draw(context, Vector(pos.x, pos.y - (strings.size() + 1) * 22 - 35 ));
+Tip::draw_up(DrawingContext& context, Vector& pos) {
+  auto position = Vector(pos.x, pos.y - (strings.size() + 1) * 22 - 35);
+  draw(context, position);
 }
 
 /* EOF */

src/editor/tip.hpp
@@ -28,8 +28,8 @@ class Tip
     Tip(GameObject* object);
     ~Tip();
 
-    void draw(DrawingContext& context, Vector pos);
-    void draw_up(DrawingContext& context, Vector pos);
+    void draw(DrawingContext& context, Vector& pos);
+    void draw_up(DrawingContext& context, Vector& pos);
 
   private:
     std::vector<std::string> strings;

src/editor/layer_icon.cpp
@@ -49,7 +49,7 @@ LayerIcon::~LayerIcon() {
 }
 
 void
-LayerIcon::draw(DrawingContext& context, Vector pos) {
+LayerIcon::draw(DrawingContext& context, const Vector& pos) {
   if (!is_valid()) return;
 
   ObjectIcon::draw(context,pos);

src/editor/layer_icon.hpp
@@ -29,7 +29,7 @@ class LayerIcon : public ObjectIcon
 
     GameObject *layer;
 
-    virtual void draw(DrawingContext& context, Vector pos);
+    virtual void draw(DrawingContext& context, const Vector& pos);
 
     int get_zpos() const;
     bool is_valid() const;

src/editor/layers_gui.cpp
@@ -62,7 +62,8 @@ void
 EditorLayersGui::draw(DrawingContext& context) {
 
   if (object_tip) {
-    object_tip->draw_up(context, get_layer_coords(hovered_layer));
+    auto position = get_layer_coords(hovered_layer);
+    object_tip->draw_up(context, position);
   }
 
   context.draw_filled_rect(Rectf(Vector(0, Ypos), Vector(Width, SCREEN_HEIGHT)),

src/editor/object_icon.cpp
@@ -64,7 +64,7 @@ ObjectIcon::calculate_offset() {
 }
 
 void
-ObjectIcon::draw(DrawingContext& context, Vector pos) {
+ObjectIcon::draw(DrawingContext& context, const Vector& pos) {
   context.draw_surface_part(surface, Rectf(Vector(0,0), surface->get_size()),
                             Rectf(pos + offset, pos + Vector(32,32) - offset), LAYER_GUI - 9);
 }

src/editor/object_icon.hpp
@@ -32,7 +32,7 @@ class ObjectIcon
     std::string object_name;
     SurfacePtr surface;
 
-    virtual void draw(DrawingContext& context, Vector pos);
+    virtual void draw(DrawingContext& context, const Vector& pos);
 
   private:
     Vector offset;

src/editor/scroller.cpp
@@ -75,7 +75,7 @@ EditorScroller::draw(DrawingContext& context) {
 }
 
 void
-EditorScroller::draw_arrow(DrawingContext& context, Vector pos) {
+EditorScroller::draw_arrow(DrawingContext& context, const Vector& pos) {
   Vector dir = pos - Vector(MIDDLE, MIDDLE);
   if (dir.x != 0 || dir.y != 0) {
     // draw a triangle

commit aa35f22718113a150ff23a9664a831eb7c6fd8c3

Fix tips not displaying correctly, regression introduced by cf8f119

src/editor/tip.cpp
@@ -56,20 +56,21 @@ Tip::~Tip() {
 }
 
 void
-Tip::draw(DrawingContext& context, Vector& pos) {
-  pos.y += 35;
-  context.draw_text(Resources::normal_font, header, pos,
+Tip::draw(DrawingContext& context, const Vector& pos) {
+  auto position = pos;
+  position.y += 35;
+  context.draw_text(Resources::normal_font, header, position,
                     ALIGN_LEFT, LAYER_GUI-11, ColorScheme::Menu::label_color);
 
   for(const auto& str : strings) {
-    pos.y += 22;
-    context.draw_text(Resources::normal_font, str, pos,
+    position.y += 22;
+    context.draw_text(Resources::normal_font, str, position,
                       ALIGN_LEFT, LAYER_GUI-11, ColorScheme::Menu::default_color);
   }
 }
 
 void
-Tip::draw_up(DrawingContext& context, Vector& pos) {
+Tip::draw_up(DrawingContext& context, const Vector& pos) {
   auto position = Vector(pos.x, pos.y - (strings.size() + 1) * 22 - 35);
   draw(context, position);
 }

src/editor/tip.hpp
@@ -28,8 +28,8 @@ class Tip
     Tip(GameObject* object);
     ~Tip();
 
-    void draw(DrawingContext& context, Vector& pos);
-    void draw_up(DrawingContext& context, Vector& pos);
+    void draw(DrawingContext& context, const Vector& pos);
+    void draw_up(DrawingContext& context, const Vector& pos);
 
   private:
     std::vector<std::string> strings;