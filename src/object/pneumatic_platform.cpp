SgtBilko76/SuperTux-3D

commit ec788e94519b3e13a635325ef31bd35b40e7e774

Fix a few conversion warnings

src/object/pneumatic_platform.cpp
@@ -96,7 +96,7 @@ PneumaticPlatform::update(float elapsed_time)
     movement = Vector(0, (start_y + offset_y) - get_pos().y);
   }
   if (this == master) {
-    int contact_diff = contacts.size() - slave->contacts.size();
+    size_t contact_diff = contacts.size() - slave->contacts.size();
     contacts.clear();
     slave->contacts.clear();
 

src/sprite/sprite.hpp
@@ -54,7 +54,7 @@ public:
   float get_fps() const
   { return action->fps; }
   /** Get current action total frames */
-  unsigned int get_frames() const
+  size_t get_frames() const
   { return action->surfaces.size(); }
   /** Get sprite's name */
   const std::string& get_name() const

src/supertux/menu/options_menu.cpp
@@ -85,7 +85,7 @@ OptionsMenu::OptionsMenu(bool complete) :
     }
     if (!magn.empty()) //magnification not in our list but accept anyway
     {
-      next_magnification = magnifications.size();
+      next_magnification = static_cast<int>(magnifications.size());
       magnifications.push_back(magn);
     }
   }

commit ea962c7a94e188d27c0eaa73a6408b89b3c2844f

Fix pneumaticplatform jumping to maximum position
Originally broken in ec788e94519b3e13a635325ef31bd35b40e7e774

src/object/pneumatic_platform.cpp
@@ -96,7 +96,7 @@ PneumaticPlatform::update(float elapsed_time)
     movement = Vector(0, (start_y + offset_y) - get_pos().y);
   }
   if (this == master) {
-    size_t contact_diff = contacts.size() - slave->contacts.size();
+    int contact_diff = static_cast<int>(contacts.size()) - static_cast<int>(slave->contacts.size());
     contacts.clear();
     slave->contacts.clear();
 