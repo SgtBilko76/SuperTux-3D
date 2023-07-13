SgtBilko76/SuperTux-3D

commit 0519fda9658439a235b6ac1e0b1a486995cf0e2e

Refactor undo/redo system to track separate object changes (#2320)
`UndoManager`, previously used by the editor, has been removed, and the undo/redo functionality has been migrated to `GameObjectManager`, where the approach taken to save and reproduce object changes is to track each `GameObject`'s data separately.

The new object tracking system can only be enabled inside sectors, which take place in the editor, and can also manually be turned on/off by the user. The maximum amount of saved changes in the undo stack can also be changed by the user. The undo stack is cleaned up when pushing to it and when setting a new maximum limit to undo stack changes.

The virtual `save_state()` and `check_state()` `GameObject` functions can be overriden, meaning that markers or more complex objects can also save other objects' states, so they can be tracked.

Closes #1108.
Fixes #1688.
Closes #1954.
Fixes #2297.

src/object/path_gameobject.cpp
@@ -19,7 +19,6 @@
 #include <optional>
 
 #include "editor/node_marker.hpp"
-#include "gui/menu_manager.hpp"
 #include "object/path.hpp"
 #include "object/path_object.hpp"
 #include "sprite/sprite.hpp"
@@ -49,26 +48,30 @@ PathStyle PathStyle_from_string(const std::string& text)
 } // namespace
 
 PathGameObject::PathGameObject() :
-  m_path(new Path),
+  m_path(new Path(*this)),
   m_style(PathStyle::NONE),
   m_edge_sprite(),
   m_node_sprite()
 {
+  m_track_undo = false;
+
   m_name = make_unique_name("path", this);
 }
 
 PathGameObject::PathGameObject(const Vector& pos) :
-  m_path(new Path(pos)),
+  m_path(new Path(pos, *this)),
   m_style(PathStyle::NONE),
   m_edge_sprite(),
   m_node_sprite()
 {
+  m_track_undo = false;
+
   m_name = make_unique_name("path", this);
 }
 
 PathGameObject::PathGameObject(const ReaderMapping& mapping, bool backward_compatibility_hack) :
   GameObject(mapping),
-  m_path(new Path),
+  m_path(new Path(*this)),
   m_style(PathStyle::NONE),
   m_edge_sprite(),
   m_node_sprite()
@@ -106,7 +109,6 @@ PathGameObject::~PathGameObject()
 void
 PathGameObject::update(float dt_sec)
 {
-  check_references();
 }
 
 void
@@ -176,12 +178,6 @@ PathGameObject::get_settings()
 }
 
 void
-PathGameObject::editor_update()
-{
-  check_references();
-}
-
-void
 PathGameObject::editor_select()
 {
   log_fatal << "PathGameObject::selected" << std::endl;
@@ -204,17 +200,6 @@ PathGameObject::remove_me()
       handle.remove_me(); // Removing a node handle also removes its bezier handles
   }
 
-  const auto& path_objects = Sector::get().get_objects_by_type<PathObject>();
-
-  for (const auto& path_obj : path_objects)
-  {
-    if (path_obj.get_path_gameobject() == this)
-    {
-      log_warning << "Attempt to delete path " << get_name() << " while bound to object" << std::endl;
-      return;
-    }
-  }
-
   GameObject::remove_me();
 }
 
@@ -224,23 +209,19 @@ PathGameObject::copy_into(PathGameObject& other)
   other.get_path().m_nodes = get_path().m_nodes;
 }
 
-void
-PathGameObject::check_references()
+bool
+PathGameObject::is_saveable() const
 {
   if (!Sector::current())
-    return;
-
-  // Object settings menu might hold references to paths
-  if (MenuManager::instance().is_active())
-    return;
+    return false;
 
   const auto& path_objects = Sector::get().get_objects_by_type<PathObject>();
 
   for (const auto& path_obj : path_objects)
     if (path_obj.get_path_gameobject() == this)
-      return;
+      return true;
 
-  remove_me();
+  return false;
 }
 
 void

src/editor/bezier_marker.cpp
@@ -17,6 +17,7 @@
 #include "editor/bezier_marker.hpp"
 
 #include "editor/node_marker.hpp"
+#include "object/path_gameobject.hpp"
 #include "supertux/sector.hpp"
 
 BezierMarker::BezierMarker(Path::Node* node, Vector* bezier_pos) :
@@ -63,6 +64,18 @@ NodeMarker*
 BezierMarker::get_parent() const
 {
   return Sector::current()->get_object_by_uid<NodeMarker>(m_parent);
+}
+
+void
+BezierMarker::save_state()
+{
+  m_node->get_parent().get_gameobject().save_state();
+}
+
+void
+BezierMarker::check_state()
+{
+  m_node->get_parent().get_gameobject().check_state();
 }
 
 /* EOF */

src/editor/bezier_marker.hpp
@@ -22,7 +22,7 @@
 
 class NodeMarker;
 
-class BezierMarker : public MarkerObject
+class BezierMarker final : public MarkerObject
 {
 public:
   BezierMarker(Path::Node* node, Vector* bezier_pos);
@@ -38,6 +38,9 @@ public:
 
   void set_parent(UID uid) { m_parent = uid; }
   NodeMarker* get_parent() const;
+
+  void save_state() override;
+  void check_state() override;
 
 private:
   Path::Node* m_node;

src/editor/editor.cpp
@@ -37,7 +37,6 @@
 #include "editor/tile_selection.hpp"
 #include "editor/tip.hpp"
 #include "editor/tool_icon.hpp"
-#include "editor/undo_manager.hpp"
 #include "gui/dialog.hpp"
 #include "gui/menu_manager.hpp"
 #include "gui/mousecursor.hpp"
@@ -108,14 +107,13 @@ Editor::Editor() :
   m_after_setup(false),
   m_tileset(nullptr),
   m_widgets(),
+  m_undo_widget(),
+  m_redo_widget(),
   m_overlay_widget(),
   m_toolbox_widget(),
   m_layers_widget(),
   m_enabled(false),
   m_bgr_surface(Surface::from_file("images/engine/menu/bg_editor.png")),
-  m_undo_manager(new UndoManager),
-  m_ignore_sector_change(false),
-  m_level_first_loaded(false),
   m_time_since_last_save(0.f),
   m_scroll_speed(32.0f)
 {
@@ -127,13 +125,6 @@ Editor::Editor() :
   m_layers_widget = layers_widget.get();
   m_overlay_widget = overlay_widget.get();
 
-  auto undo_button_widget = std::make_unique<ButtonWidget>("images/engine/editor/undo.png",
-    Vector(10, 10), [this]{ undo(); });
-  auto redo_button_widget = std::make_unique<ButtonWidget>("images/engine/editor/redo.png",
-    Vector(60, 10), [this]{ redo(); });
-
-  m_widgets.push_back(std::move(undo_button_widget));
-  m_widgets.push_back(std::move(redo_button_widget));
   m_widgets.push_back(std::move(toolbox_widget));
   m_widgets.push_back(std::move(layers_widget));
   m_widgets.push_back(std::move(overlay_widget));
@@ -292,7 +283,10 @@ Editor::save_level(const std::string& filename, bool switch_file)
   if (switch_file)
     m_levelfile = filename;
 
-  m_undo_manager->reset_index();
+  for (const auto& sector : m_level->m_sectors)
+  {
+    sector->clear_undo_stack();
+  }
   m_level->save(m_world ? FileSystem::join(m_world->get_basedir(), file) : file);
   m_time_since_last_save = 0.f;
   remove_autosave_file();
@@ -443,6 +437,10 @@ Editor::load_sector(const std::string& name)
   if (!sector) {
     sector = m_level->get_sector(0);
   }
+
+  sector->set_undo_stack_size(g_config->editor_undo_stack_size);
+  sector->toggle_undo_tracking(g_config->editor_undo_tracking);
+
   set_sector(sector);
 }
 
@@ -525,13 +523,6 @@ Editor::set_level(std::unique_ptr<Level> level, bool reset)
   m_layers_widget->refresh_sector_text();
   m_toolbox_widget->update_mouse_icon();
   m_overlay_widget->on_level_change();
-  
-  if (!m_level_first_loaded)
-  {
-    m_undo_manager->try_snapshot(*m_level);
-    m_undo_manager->reset_index();
-    m_level_first_loaded = true;
-  }
 }
 
 void
@@ -543,6 +534,9 @@ Editor::reload_level()
                                    StringUtil::has_suffix(m_levelfile, ".stwm"),
                                    true));
   ReaderMapping::s_translations_enabled = true;
+
+  retoggle_undo_tracking();
+  undo_stack_cleanup();
 
   // Autosave files : Once the level is loaded, make sure
   // to use the regular file
@@ -584,11 +578,31 @@ Editor::quit_editor()
 void
 Editor::check_unsaved_changes(const std::function<void ()>& action)
 {
-  if (m_undo_manager->has_unsaved_changes() && m_levelloaded)
+  if (!m_levelloaded)
+  {
+    action();
+    return;
+  }
+
+  bool has_unsaved_changes = !g_config->editor_undo_tracking;
+  if (!has_unsaved_changes)
+  {
+    for (const auto& sector : m_level->m_sectors)
+    {
+      if (sector->has_object_changes())
+      {
+        has_unsaved_changes = true;
+        break;
+      }
+    }
+  }
+
+  if (has_unsaved_changes)
   {
     m_enabled = false;
     auto dialog = std::make_unique<Dialog>();
-    dialog->set_text(_("This level contains unsaved changes, do you want to save?"));
+    dialog->set_text(g_config->editor_undo_tracking ? _("This level contains unsaved changes, do you want to save?") :
+                                                      _("This level may contain unsaved changes, do you want to save?"));
     dialog->add_default_button(_("Yes"), [this, action] {
       check_save_prerequisites([this, action] {
         save_level();
@@ -735,28 +749,11 @@ Editor::event(const SDL_Event& ev)
       return;
     }
 
-    m_ignore_sector_change = false;
-
     BIND_SECTOR(*m_sector);
 
     for(const auto& widget : m_widgets) {
       if (widget->event(ev))
         break;
-    }
-
-    // unreliable heuristic to snapshot the current state for future undo
-    if (((ev.type == SDL_KEYUP && ev.key.repeat == 0 &&
-         ev.key.keysym.sym != SDLK_LSHIFT &&
-         ev.key.keysym.sym != SDLK_RSHIFT &&
-         ev.key.keysym.sym != SDLK_LCTRL &&
-         ev.key.keysym.sym != SDLK_RCTRL) ||
-         ev.type == SDL_MOUSEBUTTONUP))
-    {
-      if (!m_ignore_sector_change) {
-        if (m_level) {
-          m_undo_manager->try_snapshot(*m_level);
-        }
-      }
     }
 
     // Scroll with mouse wheel, if the mouse is not over the toolbox.
@@ -872,29 +869,72 @@ Editor::check_save_prerequisites(const std::function<void ()>& callback) const
 }
 
 void
+Editor::retoggle_undo_tracking()
+{
+  if (g_config->editor_undo_tracking && !m_undo_widget)
+  {
+    // Add undo/redo button widgets.
+    auto undo_button_widget = std::make_unique<ButtonWidget>("images/engine/editor/undo.png",
+        Vector(10, 10), [this]{ undo(); });
+    auto redo_button_widget = std::make_unique<ButtonWidget>("images/engine/editor/redo.png",
+        Vector(60, 10), [this]{ redo(); });
+
+    m_undo_widget = undo_button_widget.get();
+    m_redo_widget = redo_button_widget.get();
+
+    m_widgets.insert(m_widgets.begin(), std::move(undo_button_widget));
+    m_widgets.insert(m_widgets.begin() + 1, std::move(redo_button_widget));
+  }
+  else if (!g_config->editor_undo_tracking && m_undo_widget)
+  {
+    // Remove undo/redo button widgets.
+    m_widgets.erase(std::remove_if(
+                      m_widgets.begin(), m_widgets.end(),
+                      [this](const std::unique_ptr<Widget>& widget) {
+                          const Widget* ptr = widget.get();
+                          return ptr == m_undo_widget || ptr == m_redo_widget;
+                      }), m_widgets.end());
+    m_undo_widget = nullptr;
+    m_redo_widget = nullptr;
+  }
+
+  // Toggle undo tracking for all sectors.
+  for (const auto& sector : m_level->m_sectors)
+    sector->toggle_undo_tracking(g_config->editor_undo_tracking);
+}
+
+void
+Editor::undo_stack_cleanup()
+{
+  // Set the undo stack size and perform undo stack cleanup on all sectors.
+  for (const auto& sector : m_level->m_sectors)
+  {
+    sector->set_undo_stack_size(g_config->editor_undo_stack_size);
+    sector->undo_stack_cleanup();
+  }
+}
+
+void
 Editor::undo()
 {
-  log_info << "attempting undo" << std::endl;
-  auto level = m_undo_manager->undo();
-  if (level) {
-    set_level(std::move(level), false);
-    m_ignore_sector_change = true;
-  } else {
-    log_info << "undo failed" << std::endl;
-  }
+  BIND_SECTOR(*m_sector);
+  m_sector->undo();
+  post_undo_redo_actions();
 }
 
 void
 Editor::redo()
 {
-  log_info << "attempting redo" << std::endl;
-  auto level = m_undo_manager->redo();
-  if (level) {
-    set_level(std::move(level), false);
-    m_ignore_sector_change = true;
-  } else {
-    log_info << "redo failed" << std::endl;
-  }
+  BIND_SECTOR(*m_sector);
+  m_sector->redo();
+  post_undo_redo_actions();
+}
+
+void
+Editor::post_undo_redo_actions()
+{
+  m_overlay_widget->delete_markers();
+  m_layers_widget->update_current_tip();
 }
 
 IntegrationStatus

src/editor/editor.hpp
@@ -35,6 +35,7 @@
 #include "util/string_util.hpp"
 #include "video/surface_ptr.hpp"
 
+class ButtonWidget;
 class GameObject;
 class Level;
 class ObjectGroup;
@@ -42,7 +43,6 @@ class Path;
 class Savegame;
 class Sector;
 class TileSet;
-class UndoManager;
 class World;
 
 class Editor final : public Screen,
@@ -152,6 +152,9 @@ public:
 
   Sector* get_sector() { return m_sector; }
 
+  void retoggle_undo_tracking();
+  void undo_stack_cleanup();
+
   void undo();
   void redo();
 
@@ -171,6 +174,8 @@ private:
   void save_level(const std::string& filename = "", bool switch_file = false);
   void test_level(const std::optional<std::pair<std::string, Vector>>& test_pos);
   void update_keyboard(const Controller& controller);
+
+  void post_undo_redo_actions();
 
 protected:
   std::unique_ptr<Level> m_level;
@@ -205,6 +210,8 @@ private:
   TileSet* m_tileset;
 
   std::vector<std::unique_ptr<Widget> > m_widgets;
+  ButtonWidget* m_undo_widget;
+  ButtonWidget* m_redo_widget;
   EditorOverlayWidget* m_overlay_widget;
   EditorToolboxWidget* m_toolbox_widget;
   EditorLayersWidget* m_layers_widget;
@@ -212,11 +219,6 @@ private:
   bool m_enabled;
   SurfacePtr m_bgr_surface;
 
-  std::unique_ptr<UndoManager> m_undo_manager;
-  bool m_ignore_sector_change;
-  
-  bool m_level_first_loaded;
-  
   float m_time_since_last_save;
 
   float m_scroll_speed;

src/editor/layers_widget.cpp
@@ -131,11 +131,19 @@ EditorLayersWidget::update(float dt_sec)
   {
     auto layer_icon = (*it).get();
     if (!layer_icon->is_valid())
+    {
       it = m_layer_icons.erase(it);
-    else
-      ++it;
+      continue;
+    }
+    ++it;
   }
-  
+
+  TileMap* selected_tilemap = get_selected_tilemap();
+  if (selected_tilemap)
+    selected_tilemap->m_editor_active = true;
+  else
+    refresh_layers(); // Ensure a tilemap is always selected
+
   if(m_scroll_speed < 0 && m_scroll > 0)
   {
     m_scroll -= 5;
@@ -171,13 +179,10 @@ EditorLayersWidget::on_mouse_button_down(const SDL_MouseButtonEvent& button)
         }
         else
         {
-          if (m_layer_icons[m_hovered_layer]->is_tilemap()) {
-            if (m_selected_tilemap) {
-              m_selected_tilemap->m_editor_active = false;
-            }
-            m_selected_tilemap = static_cast<TileMap*>(m_layer_icons[m_hovered_layer]->get_layer());
-            m_selected_tilemap->m_editor_active = true;
-            m_editor.edit_path(m_selected_tilemap->get_path_gameobject(), m_selected_tilemap);
+          TileMap* tilemap = dynamic_cast<TileMap*>(m_layer_icons[m_hovered_layer]->get_layer());
+          if (tilemap) {
+            set_selected_tilemap(tilemap);
+            m_editor.edit_path(tilemap->get_path_gameobject(), tilemap);
           } else {
             auto cam = dynamic_cast<Camera*>(m_layer_icons[m_hovered_layer]->get_layer());
             if (cam) {
@@ -308,39 +313,11 @@ EditorLayersWidget::setup()
 void
 EditorLayersWidget::refresh()
 {
-  m_selected_tilemap = nullptr;
   m_layer_icons.clear();
+  for (const auto& obj : m_editor.get_sector()->get_objects())
+    add_layer(obj.get(), true);
 
-  bool tsel = false;
-  TileMap* first_tm = nullptr;
-  for (auto& i : m_editor.get_sector()->get_objects())
-  {
-    auto* go = i.get();
-    auto* mo = dynamic_cast<MovingObject*>(go);
-    if (!mo && go->has_settings()) {
-      if (!dynamic_cast<PathGameObject*>(go)) {
-        add_layer(go);
-      }
-
-      auto tm = dynamic_cast<TileMap*>(go);
-      if (tm) {
-        if (first_tm == nullptr)
-          first_tm = tm;
-        if ( !tm->is_solid() || tsel ) {
-          tm->m_editor_active = false;
-        } else {
-          m_selected_tilemap = tm;
-          tm->m_editor_active = true;
-          tsel = true;
-        }
-      }
-    }
-  }
-  if (!tsel && first_tm != nullptr)
-  {
-    first_tm->m_editor_active = true;
-    m_selected_tilemap = first_tm;
-  }
+  refresh_layers();
 
   sort_layers();
   refresh_sector_text();
@@ -354,6 +331,39 @@ EditorLayersWidget::refresh_sector_text()
 }
 
 void
+EditorLayersWidget::refresh_layers()
+{
+  bool tilemap_selected = false;
+  TileMap* first_tilemap = nullptr;
+  for (const auto& icon : m_layer_icons)
+  {
+    auto* go = icon->get_layer();
+    auto tm = dynamic_cast<TileMap*>(go);
+    if (!tm)
+      continue;
+
+    if (first_tilemap == nullptr)
+    {
+      first_tilemap = tm;
+    }
+    if (!tm->is_solid() || tilemap_selected)
+    {
+      tm->m_editor_active = false;
+    }
+    else
+    {
+      set_selected_tilemap(tm);
+      tilemap_selected = true;
+    }
+  }
+
+  if (!tilemap_selected && first_tilemap != nullptr)
+  {
+    set_selected_tilemap(first_tilemap);
+  }
+}
+
+void
 EditorLayersWidget::sort_layers()
 {
   std::sort(m_layer_icons.begin(), m_layer_icons.end(),
@@ -363,21 +373,36 @@ EditorLayersWidget::sort_layers()
 }
 
 void
-EditorLayersWidget::add_layer(GameObject* layer)
+EditorLayersWidget::add_layer(GameObject* layer, bool initial)
 {
+  if (!layer->has_settings() ||
+      dynamic_cast<MovingObject*>(layer) ||
+      dynamic_cast<PathGameObject*>(layer))
+  {
+    return;
+  }
+
   auto icon = std::make_unique<LayerIcon>(layer);
   int z_pos = icon->get_zpos();
 
   // The icon is inserted to the correct position.
+  bool inserted = false;
   for (auto i = m_layer_icons.begin(); i != m_layer_icons.end(); ++i) {
     const auto& li = i->get();
-    if (li->get_zpos() < z_pos) {
+    if (li->get_zpos() > z_pos) {
       m_layer_icons.insert(i, std::move(icon));
-      return;
+      inserted = true;
+      break;
     }
   }
 
-  m_layer_icons.push_back(std::move(icon));
+  if (!inserted)
+    m_layer_icons.push_back(std::move(icon));
+
+  // Newly added tilemaps shouldn't be active
+  TileMap* tilemap = dynamic_cast<TileMap*>(layer);
+  if (tilemap)
+    tilemap->m_editor_active = false;
 }
 
 void
@@ -388,6 +413,31 @@ EditorLayersWidget::update_tip()
     return;
   }
   m_object_tip = std::make_unique<Tip>(*m_layer_icons[m_hovered_layer]->get_layer());
+}
+
+void
+EditorLayersWidget::update_current_tip()
+{
+  if (!m_object_tip) return;
+
+  update_tip();
+}
+
+TileMap*
+EditorLayersWidget::get_selected_tilemap() const
+{
+  return m_editor.get_sector()->get_object_by_uid<TileMap>(m_selected_tilemap);
+}
+
+void
+EditorLayersWidget::set_selected_tilemap(TileMap* tilemap)
+{
+  TileMap* selected_tilemap = get_selected_tilemap();
+  if (selected_tilemap)
+    selected_tilemap->m_editor_active = false;
+
+  m_selected_tilemap = tilemap->get_uid();
+  tilemap->m_editor_active = true;
 }
 
 Vector

src/editor/layers_widget.hpp
@@ -18,17 +18,18 @@
 #define HEADER_SUPERTUX_EDITOR_LAYERS_WIDGET_HPP
 
 #include <stdexcept>
+#include <memory>
 
-#include "control/input_manager.hpp"
 #include "editor/widget.hpp"
+
 #include "math/fwd.hpp"
-#include "object/tilemap.hpp"
-#include "supertux/screen.hpp"
+#include "util/uid.hpp"
 
 class DrawingContext;
 class Editor;
 class GameObject;
 class LayerIcon;
+class TileMap;
 class Tip;
 
 /** A widget at the bottom of the screen for switching between tilemap
@@ -57,14 +58,18 @@ public:
   void refresh();
 
   void refresh_sector_text();
+  void refresh_layers();
   void sort_layers();
-  void add_layer(GameObject* layer);
+  void add_layer(GameObject* layer, bool initial = false);
+
+  void update_current_tip();
 
   bool has_mouse_focus() const;
 
-  TileMap* get_selected_tilemap() const { return m_selected_tilemap; }
+  TileMap* get_selected_tilemap() const;
 
 private:
+  void set_selected_tilemap(TileMap* tilemap);
   Vector get_layer_coords(const int pos) const;
   int get_layer_pos(const Vector& coords) const;
   void update_tip();
@@ -72,7 +77,7 @@ private:
 private:
   Editor& m_editor;
   std::vector<std::unique_ptr<LayerIcon>> m_layer_icons;
-  TileMap* m_selected_tilemap;
+  UID m_selected_tilemap;
 
   int m_Ypos;
   const int m_Xpos = 32;

src/editor/marker_object.hpp
@@ -39,8 +39,12 @@ public:
   virtual bool hide_if_no_offset() const { return false; }
 
   virtual bool is_saveable() const override { return false; }
+  bool track_state() const override { return false; }
 
   virtual int get_layer() const override { return LAYER_GUI - 20; }
+
+  virtual void save_state() override = 0;
+  virtual void check_state() override = 0;
 
 private:
   MarkerObject(const MarkerObject&) = delete;

commit 153d67cf15e7d36aa84cc77fd844eaeede17dc40

PathGameObject saveable check now checks for references in all sectors
Fixes an issue, introduced in #2320, regarding the `PathGameObject::is_saveable()` function's implementation.

The issue caused paths to not be saveable, if the current sector, opened in the editor, does not have any PathObjects that reference them. The check is now performed for all sectors in a level.

src/object/path_gameobject.cpp
@@ -24,6 +24,7 @@
 #include "sprite/sprite.hpp"
 #include "sprite/sprite_manager.hpp"
 #include "supertux/debug.hpp"
+#include "supertux/level.hpp"
 #include "supertux/sector.hpp"
 #include "util/log.hpp"
 #include "util/reader_mapping.hpp"
@@ -215,11 +216,12 @@ PathGameObject::is_saveable() const
   if (!Sector::current())
     return false;
 
-  const auto& path_objects = Sector::get().get_objects_by_type<PathObject>();
-
-  for (const auto& path_obj : path_objects)
-    if (path_obj.get_path_gameobject() == this)
-      return true;
+  for (const auto& sector : Level::current()->get_sectors())
+  {
+    for (const auto& path_obj : sector->get_objects_by_type<PathObject>())
+      if (path_obj.get_path_gameobject() == this)
+        return true;
+  }
 
   return false;
 }

src/supertux/level.hpp
@@ -52,6 +52,7 @@ public:
 
   size_t get_sector_count() const;
   Sector* get_sector(size_t num) const;
+  const std::vector<std::unique_ptr<Sector> >& get_sectors() const { return m_sectors; }
 
   std::string get_tileset() const { return m_tileset; }
 