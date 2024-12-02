SgtBilko76/SuperTux-3D

commit 6b13c3457908673df1f3df851b5654fe8b2d649c

Tilegroups, imported autotilesets and thunderstorm tiles now support offsetting
Tilegroups from an imported tileset are now parsed even if a start, end or offset are specified. If a tile ID doesn't fit the specified range, it is replaced by 0.

Additionally:

* "offset" can be provided under "import-autotileset" to offset all tile IDs in the imported autotileset file, also keeping in mind the current tileset offset.
* Imported autotilesets will not have any autotiles parsed, which have an ID that doesn't fit the range of the current tileset.
* `start`, `end` and `offset` are now member variables in both `TileSetParser` and `AutotileParser`.

src/supertux/tile_set_parser.cpp
@@ -31,27 +31,34 @@
 #include "util/file_system.hpp"
 #include "video/surface.hpp"
 
-TileSetParser::TileSetParser(TileSet& tileset, const std::string& filename) :
+TileSetParser::TileSetParser(TileSet& tileset, const std::string& filename,
+                             int32_t start, int32_t end, int32_t offset) :
   m_tileset(tileset),
   m_filename(filename),
-  m_tiles_path()
+  m_tiles_path(),
+  m_start(start),
+  m_end(end),
+  m_offset(offset)
 {
 }
 
 void
-TileSetParser::parse(int32_t start, int32_t end, int32_t offset, bool imported)
+TileSetParser::parse(bool imported)
 {
-  if (offset && start + offset < 1) {
-    start = -offset + 1;
-    log_warning << "The defined offset would assign non-positive ids to tiles, tiles below " << -offset + 1 << " will be ignored." << std::endl;
+  if (m_offset && m_start + m_offset < 1)
+  {
+    m_start = -m_offset + 1;
+    log_warning << "The defined offset would assign non-positive IDs to tiles, tiles below " << -m_offset + 1 << " will be ignored." << std::endl;
   }
-  if (end < 0) {
+  if (m_end < 0)
+  {
     log_warning << "Cannot import tiles with negative IDs." << std::endl;
     return;
   }
-  if (start < 0) {
+  if (m_start < 0)
+  {
     log_warning << "Cannot import tiles with negative IDs. Importing will start at ID 1." << std::endl;
-    start = 1;
+    m_start = 1;
   }
 
   m_tiles_path = FileSystem::dirname(m_filename);
@@ -68,14 +75,11 @@ TileSetParser::parse(int32_t start, int32_t end, int32_t offset, bool imported)
   {
     if (iter.get_key() == "tile")
     {
-      ReaderMapping tile_mapping = iter.as_mapping();
-      parse_tile(tile_mapping, start, end, offset);
+      parse_tile(iter.as_mapping());
     }
     else if (iter.get_key() == "tilegroup")
     {
       /* tilegroups are only interesting for the editor */
-      /* ignore tilegroups, unless there's no limit and offset provided */
-      if (start || end || offset) continue;
       ReaderMapping reader = iter.as_mapping();
       Tilegroup tilegroup;
       reader.get("name", tilegroup.name);
@@ -83,56 +87,93 @@ TileSetParser::parse(int32_t start, int32_t end, int32_t offset, bool imported)
 
       // Allow offsetting every tile ID, specified in the tilegroup
       int32_t tiles_offset = 0;
-      if (reader.get("offset", tiles_offset))
-        for (int& tile : tilegroup.tiles)
-          if (tile != 0) tile += tiles_offset;
+      reader.get("offset", tiles_offset);
 
-      m_tileset.add_tilegroup(tilegroup);
+      bool has_valid_tile = false;
+      if (m_offset || tiles_offset)
+      {
+        for (int& tile : tilegroup.tiles)
+        {
+          if (tile == 0) continue;
+
+          if (tile < m_start || (m_end && tile > m_end))
+          {
+            tile = 0;
+          }
+          else
+          {
+            tile += m_offset + tiles_offset;
+            has_valid_tile = true;
+          }
+        }
+      }
+
+      if (has_valid_tile)
+      {
+        m_tileset.add_tilegroup(std::move(tilegroup));
+      }
     }
     else if (iter.get_key() == "tiles")
     {
-      ReaderMapping tiles_mapping = iter.as_mapping();
-      parse_tiles(tiles_mapping, start, end, offset);
+      parse_tiles(iter.as_mapping());
     }
     else if (iter.get_key() == "autotileset")
     {
-      /* ignore autotiles, unless there's no limit and offset provided */
-      if (start || end || offset) continue;
       ReaderMapping reader = iter.as_mapping();
       std::string autotile_filename;
       if (!reader.get("source", autotile_filename))
       {
         log_warning << "No source path for autotiles in file '" << m_filename << "'" << std::endl;
+        continue;
       }
-      else
-      {
-        AutotileParser parser(m_tileset.m_autotilesets,
-            FileSystem::normalize(m_tiles_path + autotile_filename));
-        parser.parse();
-      }
+
+      int32_t import_offset = 0;
+      reader.get("offset", import_offset);
+
+      AutotileParser parser(m_tileset.m_autotilesets,
+          FileSystem::normalize(m_tiles_path + autotile_filename),
+          m_start, m_end, import_offset + m_offset);
+      parser.parse();
     }
     else if (iter.get_key() == "import-tileset")
     {
       ReaderMapping reader = iter.as_mapping();
       std::string import_filename;
+      if (!reader.get("file", import_filename))
+      {
+        log_warning << "No source path for imported tileset in file '" << m_filename << "'" << std::endl;
+        continue;
+      }
+      if (import_filename == m_filename)
+      {
+        log_warning << "Tried to recursively import tileset '" << m_filename << "'" << std::endl;
+        continue;
+      }
+
       int32_t import_start = 0, import_end = 0, import_offset = 0;
-      reader.get("file", import_filename);
       reader.get("start", import_start);
       reader.get("end", import_end);
       reader.get("offset", import_offset);
-      if (import_start + import_offset < start) {
-        import_start = (start - import_offset) < 0 ? 0 : (start - import_offset);
+
+      import_offset += m_offset;
+
+      if (import_start + import_offset < m_start)
+      {
+        import_start = std::max(0, m_start - import_offset);
       }
-      if (end && (!import_end || (import_end + import_offset) > end)) {
-        import_end = end - import_offset;
+      if (m_end && (!import_end || import_end + import_offset > m_end))
+      {
+        import_end = m_end - import_offset;
       }
-      if (import_end < import_start) {
+      if (import_end < import_start)
+      {
         if (!imported) log_warning << "The defined range has a negative size, no tiles will be imported." << std::endl;
         continue;
       }
-      import_offset += offset;
-      TileSetParser import_parser(m_tileset, import_filename);
-      import_parser.parse(import_start, import_end, import_offset, true);
+
+      TileSetParser parser(m_tileset, import_filename,
+            import_start, import_end, import_offset);
+      parser.parse(true);
     }
     else if (iter.get_key() == "additional")
     {
@@ -155,7 +196,11 @@ TileSetParser::parse(int32_t start, int32_t end, int32_t offset, bool imported)
             if (tiles.size() % 2 != 0) tiles.pop_back(); // If the number of tiles isn't even, remove last tile.
             for (int i = 0; i < static_cast<int>(tiles.size()); i += 2)
             {
-              m_tileset.m_thunderstorm_tiles.insert({tiles[i], tiles[i + 1]});
+              if (tiles[i] < m_start || (m_end && tiles[i] > m_end) ||
+                  tiles[i + 1] < m_start || (m_end && tiles[i + 1] > m_end))
+                continue; // Both tiles have to fit in the tile range.
+
+              m_tileset.m_thunderstorm_tiles.insert({ tiles[i] + m_offset, tiles[i + 1] + m_offset });
             }
           }
         }
@@ -180,15 +225,15 @@ TileSetParser::parse(int32_t start, int32_t end, int32_t offset, bool imported)
 }
 
 void
-TileSetParser::parse_tile(const ReaderMapping& reader, int32_t min, int32_t max, int32_t offset)
+TileSetParser::parse_tile(const ReaderMapping& reader)
 {
   uint32_t id;
   if (!reader.get("id", id))
   {
     throw std::runtime_error("Missing tile-id.");
   }
-  if (max && (id < static_cast<uint32_t>(min) || id > static_cast<uint32_t>(max))) return;
-  id += offset;
+  if (id < static_cast<uint32_t>(m_start) || (m_end && id > static_cast<uint32_t>(m_end))) return;
+  id += m_offset;
 
   uint32_t attributes = 0;
 
@@ -265,7 +310,7 @@ TileSetParser::parse_tile(const ReaderMapping& reader, int32_t min, int32_t max,
 }
 
 void
-TileSetParser::parse_tiles(const ReaderMapping& reader, int32_t min, int32_t max, int32_t offset)
+TileSetParser::parse_tiles(const ReaderMapping& reader)
 {
   // List of ids (use 0 if the tile should be ignored)
   std::vector<uint32_t> ids;
@@ -300,7 +345,6 @@ TileSetParser::parse_tiles(const ReaderMapping& reader, int32_t min, int32_t max
   // Allow specifying additional offset to tiles
   int32_t tiles_offset = 0;
   reader.get("offset", tiles_offset);
-  offset += tiles_offset;
 
   bool deprecated = false;
   reader.get("deprecated", deprecated);
@@ -362,8 +406,8 @@ TileSetParser::parse_tiles(const ReaderMapping& reader, int32_t min, int32_t max
 
       for (size_t i = 0; i < ids.size(); ++i)
       {
-        if (!ids[i] || (max && (ids[i] < static_cast<uint32_t>(min) || ids[i] > static_cast<uint32_t>(max)))) continue;
-        ids[i] += offset;
+        if (!ids[i] || (ids[i] < static_cast<uint32_t>(m_start) || (m_end && ids[i] > static_cast<uint32_t>(m_end)))) continue;
+        ids[i] += m_offset + tiles_offset;
 
         const int x = static_cast<int>(32 * (i % width));
         const int y = static_cast<int>(32 * (i / width));
@@ -395,8 +439,8 @@ TileSetParser::parse_tiles(const ReaderMapping& reader, int32_t min, int32_t max
     {
       for (size_t i = 0; i < ids.size(); ++i)
       {
-        if(!ids[i] || (max && (ids[i] < static_cast<uint32_t>(min) || ids[i] > static_cast<uint32_t>(max)))) continue;
-        ids[i] += offset;
+        if(!ids[i] || (ids[i] < static_cast<uint32_t>(m_start) || (m_end && ids[i] > static_cast<uint32_t>(m_end)))) continue;
+        ids[i] += m_offset + tiles_offset;
 
         int x = static_cast<int>(32 * (i % width));
         int y = static_cast<int>(32 * (i / width));

src/supertux/autotile.cpp
@@ -271,7 +271,7 @@ AutotileSet::get_mask_from_tile(uint32_t tile) const
 }
 
 void
-AutotileSet::validate() const
+AutotileSet::validate(int32_t start, int32_t end) const
 {
   // Corner autotiles are always empty if all 4 corners are, but regular
   // autotiles should have a valid tile ID that can be surrounded by emptiness.
@@ -288,7 +288,9 @@ AutotileSet::validate() const
       {
         if (tile_exists)
         {
-          log_warning << "Autotileset '" << m_name << "': mask " << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " corresponds both to tile " << tile_with_that_mask << " and " << autotile->get_tile_id() << std::endl;
+          log_warning << "Autotileset '" << m_name
+                      << "' (range " << (start ? std::to_string(start) : "...") << "-" << (end ? std::to_string(end) : "...") << "): mask "
+                      << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " corresponds both to tile " << tile_with_that_mask << " and " << autotile->get_tile_id() << std::endl;
         }
         else
         {
@@ -301,7 +303,9 @@ AutotileSet::validate() const
       {
         if (tile_nonsolid)
         {
-          log_warning << "Autotileset '" << m_name << "': non-solid mask " << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " corresponds both to tile " << tile_with_that_mask << " and " << autotile->get_tile_id() << std::endl;
+          log_warning << "Autotileset '" << m_name
+                      << "' (range " << (start ? std::to_string(start) : "...") << "-" << (end ? std::to_string(end) : "...") << "): non-solid mask "
+                      << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " corresponds both to tile " << tile_with_that_mask << " and " << autotile->get_tile_id() << std::endl;
         }
         else
         {
@@ -312,7 +316,9 @@ AutotileSet::validate() const
 
     if (!tile_exists)
     {
-      log_warning << "Autotileset '" << m_name << "': mask " << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " has no corresponding tile" << std::endl;
+      log_warning << "Autotileset '" << m_name
+                  << "' (range " << (start ? std::to_string(start) : "...") << "-" << (end ? std::to_string(end) : "...") << "): mask "
+                  << (m_corner ? std::bitset<4>(mask).to_string() : std::bitset<8>(mask).to_string()) << " has no corresponding tile" << std::endl;
     }
   }
 }

src/supertux/autotile.hpp
@@ -118,7 +118,7 @@ public:
 
   // TODO : Validate autotile config files by checking if each mask has
   //        one and only one corresponding tile.
-  void validate() const;
+  void validate(int32_t start, int32_t end) const;
 
 public:
   static std::vector<std::unique_ptr<AutotileSet>> m_autotilesets;

src/supertux/autotile_parser.cpp
@@ -27,16 +27,36 @@
 #include "util/reader_mapping.hpp"
 #include "util/file_system.hpp"
 
-AutotileParser::AutotileParser(std::vector<std::unique_ptr<AutotileSet>>& autotilesets, const std::string& filename) :
+AutotileParser::AutotileParser(std::vector<std::unique_ptr<AutotileSet>>& autotilesets, const std::string& filename,
+                               int32_t start, int32_t end, int32_t offset) :
   m_autotilesets(autotilesets),
   m_filename(filename),
-  m_tiles_path()
+  m_tiles_path(),
+  m_start(start),
+  m_end(end),
+  m_offset(offset)
 {
 }
 
 void
 AutotileParser::parse()
 {
+  if (m_offset && m_start + m_offset < 1)
+  {
+    m_start = -m_offset + 1;
+    log_warning << "The defined offset would assign non-positive IDs to autotiles, autotiles below " << -m_offset + 1 << " will be ignored." << std::endl;
+  }
+  if (m_end < 0)
+  {
+    log_warning << "Cannot import autotiles with negative IDs." << std::endl;
+    return;
+  }
+  if (m_start < 0)
+  {
+    log_warning << "Cannot import autotiles with negative IDs. Importing will start at ID 1." << std::endl;
+    m_start = 1;
+  }
+
   m_tiles_path = FileSystem::dirname(m_filename);
 
   auto doc = ReaderDocument::from_file(m_filename);
@@ -51,13 +71,11 @@ AutotileParser::parse()
   {
     if (iter.get_key() == "autotileset")
     {
-      ReaderMapping tile_mapping = iter.as_mapping();
-      parse_autotileset(tile_mapping, false);
+      parse_autotileset(iter.as_mapping(), false);
     }
     else if (iter.get_key() == "autotileset-corner")
     {
-      ReaderMapping tile_mapping = iter.as_mapping();
-      parse_autotileset(tile_mapping, true);
+      parse_autotileset(iter.as_mapping(), true);
     }
     else
     {
@@ -83,13 +101,19 @@ AutotileParser::parse_autotileset(const ReaderMapping& reader, bool corner)
     log_warning << "No default tile for autotileset " << name << std::endl;
   }
 
+  if (default_id < m_start || (m_end && default_id > m_end))
+    default_id = 0;
+  else
+    default_id += m_offset;
+
   auto iter = reader.get_iter();
   while (iter.next())
   {
     if (iter.get_key() == "autotile")
     {
-      ReaderMapping tile_mapping = iter.as_mapping();
-      autotiles.push_back(parse_autotile(tile_mapping, corner));
+      Autotile* autotile = parse_autotile(iter.as_mapping(), corner);
+      if (autotile)
+        autotiles.push_back(autotile);
     }
     else if (iter.get_key() != "name" && iter.get_key() != "default")
     {
@@ -97,11 +121,14 @@ AutotileParser::parse_autotileset(const ReaderMapping& reader, bool corner)
     }
   }
 
-  std::unique_ptr<AutotileSet> autotileset = std::make_unique<AutotileSet>(autotiles, default_id, name, corner);
+  if (autotiles.empty())
+    return;
+
+  std::unique_ptr<AutotileSet> autotileset = std::make_unique<AutotileSet>(std::move(autotiles), default_id, name, corner);
 
   if (g_config->developer_mode)
   {
-    autotileset->validate();
+    autotileset->validate(m_start, m_end);
   }
 
   m_autotilesets.push_back(std::move(autotileset));
@@ -119,6 +146,11 @@ AutotileParser::parse_autotile(const ReaderMapping& reader, bool corner)
     throw std::runtime_error("Missing 'id' parameter in autotileset config file.");
   }
 
+  if (tile_id < m_start || (m_end && tile_id > m_end))
+    return nullptr;
+
+  tile_id += m_offset;
+
   bool solid;
   if (!reader.get("solid", solid))
   {
@@ -128,7 +160,7 @@ AutotileParser::parse_autotile(const ReaderMapping& reader, bool corner)
   else
   {
     if (corner)
-      throw std::runtime_error("'solid' parameter not needed for corner-based tiles in autotileset config file.");
+      log_warning << "'solid' parameter not needed for corner-based tiles in autotileset config file." << std::endl;
   }
 
   auto iter = reader.get_iter();
@@ -156,12 +188,19 @@ AutotileParser::parse_autotile(const ReaderMapping& reader, bool corner)
       if (!alt_reader.get("id", alt_id))
       {
         log_warning << "No alt tile for autotileset" << std::endl;
+        continue;
       }
+
+      if (alt_id < m_start || (m_end && alt_id > m_end))
+        continue;
+
+      alt_id += m_offset;
 
       float weight = 0.0f;
       if (!alt_reader.get("weight", weight))
       {
         log_warning << "No weight for alt tile id" << std::endl;
+        continue;
       }
 
       if (alt_id != 0 && weight != 0.0f)
@@ -175,7 +214,7 @@ AutotileParser::parse_autotile(const ReaderMapping& reader, bool corner)
     }
   }
 
-  return new Autotile(tile_id, alt_ids, autotile_masks, solid);
+  return new Autotile(tile_id, std::move(alt_ids), std::move(autotile_masks), solid);
 }
 
 void

src/supertux/autotile_parser.hpp
@@ -31,8 +31,13 @@ private:
   std::string m_filename;
   std::string m_tiles_path;
 
+  int32_t m_start;
+  const int32_t m_end;
+  const int32_t m_offset;
+
 public:
-  AutotileParser(std::vector<std::unique_ptr<AutotileSet>>& autotilesets, const std::string& filename);
+  AutotileParser(std::vector<std::unique_ptr<AutotileSet>>& autotilesets, const std::string& filename,
+                 int32_t start = 0, int32_t end = 0, int32_t offset = 0);
 
   void parse();
 

src/supertux/tile_set_parser.hpp
@@ -35,14 +35,19 @@ private:
   std::string m_filename;
   std::string m_tiles_path;
 
-public:
-  TileSetParser(TileSet& tileset, const std::string& filename);
+  int32_t m_start;
+  const int32_t m_end;
+  const int32_t m_offset;
 
-  void parse(int32_t start = 0, int32_t end = 0, int32_t offset = 0, bool imported = false);
+public:
+  TileSetParser(TileSet& tileset, const std::string& filename,
+                int32_t start = 0, int32_t end = 0, int32_t offset = 0);
+
+  void parse(bool imported = false);
 
 private:
-  void parse_tile(const ReaderMapping& reader, int32_t min, int32_t max, int32_t offset);
-  void parse_tiles(const ReaderMapping& reader, int32_t min, int32_t max, int32_t offset);
+  void parse_tile(const ReaderMapping& reader);
+  void parse_tiles(const ReaderMapping& reader);
   std::vector<SurfacePtr> parse_imagespecs(const ReaderMapping& cur,
                                            const std::optional<Rect>& region = std::nullopt) const;
 

commit f9b23c37bcc9b368d7fca607369ff00f548f03fe

Fix another comparison of integers of different signs `int32_t` and `uint32_t`
Fixes a warning, caused by 6b13c3457908673df1f3df851b5654fe8b2d649c.

src/supertux/tile_set_parser.cpp
@@ -196,8 +196,8 @@ TileSetParser::parse(bool imported)
             if (tiles.size() % 2 != 0) tiles.pop_back(); // If the number of tiles isn't even, remove last tile.
             for (int i = 0; i < static_cast<int>(tiles.size()); i += 2)
             {
-              if (tiles[i] < m_start || (m_end && tiles[i] > m_end) ||
-                  tiles[i + 1] < m_start || (m_end && tiles[i + 1] > m_end))
+              if (tiles[i] < static_cast<uint32_t>(m_start) || (m_end && tiles[i] > static_cast<uint32_t>(m_end)) ||
+                  tiles[i + 1] < static_cast<uint32_t>(m_start) || (m_end && tiles[i + 1] > static_cast<uint32_t>(m_end)))
                 continue; // Both tiles have to fit in the tile range.
 
               m_tileset.m_thunderstorm_tiles.insert({ tiles[i] + m_offset, tiles[i + 1] + m_offset });