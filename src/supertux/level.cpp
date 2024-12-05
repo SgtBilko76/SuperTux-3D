SgtBilko76/SuperTux-3D

commit d74a9e2141e01119d9265c8c51e8f92d191fe9af

Toggling level statistics (#3063)
The coins, badguys and secrets level statistics can now be toggled from a level's settings. By default, coins and secrets are enabled, but badguys are not (due to a recent discussion regarding the difficulty of making it possible to kill all badguys in a level; can be discussed further).

A level is shown as "perfect" on a worldmap if only the enabled statistics are maxed out.

All code which draws statistics has been updated to incorporate this change as well. Statistics which are turned off will not be shown alongside others.

src/supertux/level.cpp
@@ -68,12 +68,12 @@ Level::~Level()
 }
 
 void
-Level::initialize()
+Level::initialize(const Statistics::Preferences& stat_preferences)
 {
   if (m_sectors.empty())
     throw std::runtime_error("Level has no sectors!");
 
-  m_stats.init(*this);
+  m_stats.init(*this, stat_preferences);
 
   Savegame* savegame = (GameSession::current() && !Editor::current() ?
     &GameSession::current()->get_savegame() : nullptr);
@@ -194,6 +194,13 @@ Level::save(Writer& writer)
 
   if (!m_wmselect_bkg.empty())
     writer.write("bkg", m_wmselect_bkg);
+
+  if (!m_is_worldmap)
+  {
+    writer.start_list("statistics");
+    m_stats.get_preferences().write(writer);
+    writer.end_list("statistics");
+  }
 
   for (auto& sector : m_sectors) {
     sector->save(writer);

src/supertux/level.hpp
@@ -70,7 +70,7 @@ public:
   const std::string& get_license() const { return m_license; }
 
 private:
-  void initialize();
+  void initialize(const Statistics::Preferences& stat_preferences);
 
   void save(Writer& writer);
   void load_old_format(const ReaderMapping& reader);

src/supertux/level_parser.cpp
@@ -153,13 +153,17 @@ LevelParser::load(const ReaderDocument& doc)
     throw std::runtime_error("file is not a supertux-level file.");
 
   auto level = root.get_mapping();
+  Statistics::Preferences stat_preferences;
 
   int version = 1;
   level.get("version", version);
-  if (version == 1) {
+  if (version == 1)
+  {
     log_info << "[" << doc.get_filename() << "] level uses old format: version 1" << std::endl;
     load_old_format(level);
-  } else if (version == 2 || version == 3) {
+  }
+  else if (version == 2 || version == 3)
+  {
     level.get("tileset", m_level.m_tileset);
 
     level.get("name", m_level.m_name);
@@ -172,6 +176,10 @@ LevelParser::load(const ReaderDocument& doc)
     level.get("icon", m_level.m_icon);
     level.get("icon-locked", m_level.m_icon_locked);
     level.get("bkg", m_level.m_wmselect_bkg);
+
+    std::optional<ReaderMapping> level_stat_preferences;
+    if (level.get("statistics", level_stat_preferences))
+      stat_preferences.parse(*level_stat_preferences);
 
     auto iter = level.get_iter();
     while (iter.next())
@@ -189,11 +197,13 @@ LevelParser::load(const ReaderDocument& doc)
                   << m_level.m_name << "\". You might not be allowed to share it."
                   << std::endl;
     }
-  } else {
+  }
+  else
+  {
     log_warning << "[" << doc.get_filename() << "] level format version " << version << " is not supported" << std::endl;
   }
 
-  m_level.initialize();
+  m_level.initialize(stat_preferences);
 }
 
 void
@@ -204,8 +214,6 @@ LevelParser::load_old_format(const ReaderMapping& reader)
 
   auto sector = SectorParser::from_reader_old_format(m_level, reader, m_editable);
   m_level.add_sector(std::move(sector));
-
-  m_level.initialize();
 }
 
 void

src/supertux/statistics.hpp
@@ -25,6 +25,9 @@
 
 class DrawingContext;
 class Level;
+class Menu;
+class ReaderMapping;
+class Writer;
 
 namespace ssq {
 class Table;
@@ -43,8 +46,25 @@ private:
 public:
   static std::string coins_to_string(int coins, int total_coins);
   static std::string frags_to_string(int badguys, int total_badguys);
-  static std::string time_to_string(float time);
   static std::string secrets_to_string(int secrets, int total_secrets);
+  static std::string time_to_string(float time);
+
+public:
+  class Preferences final
+  {
+  public:
+    Preferences();
+
+    void parse(const ReaderMapping& reader);
+    void write(Writer& writer) const;
+
+    void add_to_menu(Menu& menu);
+
+  public:
+    bool enable_coins;
+    bool enable_badguys;
+    bool enable_secrets;
+  };
 
 public:
   enum Status { INVALID, ACCUMULATING, FINAL };
@@ -58,6 +78,9 @@ public:
   /** unserialize statistics object from squirrel table "statistics" */
   void unserialize_from_squirrel(const ssq::Table& table);
 
+  const Preferences& get_preferences() const { return m_preferences; }
+  void add_preferences_to_menu(Menu& menu);
+
   void draw_worldmap_info(DrawingContext& context, float target_time); /**< draw worldmap stat HUD */
   void draw_endseq_panel(DrawingContext& context, Statistics* best_stats, const SurfacePtr& backdrop, float target_time); /**< draw panel shown during level's end sequence */
   void draw_ingame_stats(DrawingContext& context, bool on_pause_menu); /**< draw in-game stats */
@@ -65,12 +88,12 @@ public:
   /** Updates the timers for in-game stats rendering. Should be used from the same object that calls draw_ingame_stats(). */
   void update_timers(float dt_sec);
 
-  void init(const Level& level);
+  void init(const Level& level, const Preferences& preferences);
   void finish(float time);
   void invalidate();
 
   void update(const Statistics& stats); /**< Given another Statistics object finds the best of each one */
-  bool completed(const Statistics& stats, const float target_time) const; /* Check if stats match total stats */
+  bool completed(const float target_time) const; /* Check if stats match total stats */
 
   int get_coins() const { return m_coins; }
   int get_badguys() const { return m_badguys; }
@@ -111,6 +134,8 @@ private:
   float m_coins_time,
         m_badguys_time,
         m_secrets_time;
+
+  Preferences m_preferences;
 
 private:
   int m_max_width; /** < Gets the max width of a stats line, 255 by default */

src/supertux/levelintro.cpp
@@ -178,15 +178,25 @@ LevelIntro::draw(Compositor& compositor)
 
     py += static_cast<int>(Resources::normal_font->get_height());
 
-    draw_stats_line(context, py, _("Coins"),
-                    Statistics::coins_to_string(m_best_level_statistics->get_coins(), stats.m_total_coins),
-                    m_best_level_statistics->get_coins() >= stats.m_total_coins);
-    draw_stats_line(context, py, _("Badguys killed"),
-                    Statistics::frags_to_string(m_best_level_statistics->get_badguys(), stats.m_total_badguys),
-                    m_best_level_statistics->get_badguys() >= stats.m_total_badguys);
-    draw_stats_line(context, py, _("Secrets"),
-                    Statistics::secrets_to_string(m_best_level_statistics->get_secrets(), stats.m_total_secrets),
-                    m_best_level_statistics->get_secrets() >= stats.m_total_secrets);
+    const Statistics::Preferences& preferences = m_level.m_stats.get_preferences();
+    if (preferences.enable_coins)
+    {
+      draw_stats_line(context, py, _("Coins"),
+                      Statistics::coins_to_string(m_best_level_statistics->get_coins(), stats.m_total_coins),
+                      m_best_level_statistics->get_coins() >= stats.m_total_coins);
+    }
+    if (preferences.enable_badguys)
+    {
+      draw_stats_line(context, py, _("Badguys killed"),
+                      Statistics::frags_to_string(m_best_level_statistics->get_badguys(), stats.m_total_badguys),
+                      m_best_level_statistics->get_badguys() >= stats.m_total_badguys);
+    }
+    if (preferences.enable_secrets)
+    {
+      draw_stats_line(context, py, _("Secrets"),
+                      Statistics::secrets_to_string(m_best_level_statistics->get_secrets(), stats.m_total_secrets),
+                      m_best_level_statistics->get_secrets() >= stats.m_total_secrets);
+    }
 
     bool targetTimeBeaten = m_level.m_target_time == 0.0f || (m_best_level_statistics->get_time() != 0.0f && m_best_level_statistics->get_time() < m_level.m_target_time);
     draw_stats_line(context, py, _("Best time"),

src/supertux/menu/editor_level_menu.cpp
@@ -37,8 +37,12 @@ EditorLevelMenu::EditorLevelMenu() :
   add_textfield(_("Level Note"), &(level->m_note));
   add_file(_("Tileset"), &(level->m_tileset), std::vector<std::string>(1, ".strf"), {}, true);
 
-  if (!is_worldmap) {
+  if (!is_worldmap)
+  {
     add_floatfield(_("Target Time"), &(level->m_target_time));
+
+    add_hl();
+    level->m_stats.add_preferences_to_menu(*this);
   }
 
   add_hl();

src/worldmap/worldmap_sector.cpp
@@ -474,7 +474,7 @@ WorldMapSector::finished_level(Level* gamelevel)
   // deal with statistics
   level->get_statistics().update(gamelevel->m_stats);
 
-  if (level->get_statistics().completed(level->get_statistics(), level->get_target_time())) {
+  if (level->get_statistics().completed(level->get_target_time())) {
     level->set_perfect(true);
   }
 

commit 9f43176cd25a03ce41c48b35c3562627f07756cb

Read level "statistics" properties for worldmap level tiles
Fixes #3106. Issue introduced by d74a9e2141e01119d9265c8c51e8f92d191fe9af.

src/supertux/level.cpp
@@ -70,12 +70,12 @@ Level::~Level()
 }
 
 void
-Level::initialize(const Statistics::Preferences& stat_preferences)
+Level::initialize()
 {
   if (m_sectors.empty())
     throw std::runtime_error("Level has no sectors!");
 
-  m_stats.init(*this, stat_preferences);
+  m_stats.init(*this);
 
   Savegame* savegame = (GameSession::current() && !Editor::is_active() ?
     &GameSession::current()->get_savegame() : nullptr);

src/supertux/level.hpp
@@ -71,7 +71,7 @@ public:
   inline const std::string& get_license() const { return m_license; }
 
 private:
-  void initialize(const Statistics::Preferences& stat_preferences);
+  void initialize();
 
   void save(Writer& writer);
   void load_old_format(const ReaderMapping& reader);

src/supertux/level_parser.cpp
@@ -153,7 +153,6 @@ LevelParser::load(const ReaderDocument& doc)
     throw std::runtime_error("file is not a supertux-level file.");
 
   auto level = root.get_mapping();
-  Statistics::Preferences stat_preferences;
 
   int version = 1;
   level.get("version", version);
@@ -183,7 +182,7 @@ LevelParser::load(const ReaderDocument& doc)
 
     std::optional<ReaderMapping> level_stat_preferences;
     if (level.get("statistics", level_stat_preferences))
-      stat_preferences.parse(*level_stat_preferences);
+      m_level.m_stats.get_preferences().parse(*level_stat_preferences);
 
     auto iter = level.get_iter();
     while (iter.next())
@@ -207,7 +206,7 @@ LevelParser::load(const ReaderDocument& doc)
     log_warning << "[" << doc.get_filename() << "] level format version " << version << " is not supported" << std::endl;
   }
 
-  m_level.initialize(stat_preferences);
+  m_level.initialize();
 }
 
 void

src/supertux/statistics.cpp
@@ -493,7 +493,7 @@ Statistics::update_timers(float dt_sec)
 }
 
 void
-Statistics::init(const Level& level, const Statistics::Preferences& preferences)
+Statistics::init(const Level& level)
 {
   m_status = ACCUMULATING;
 
@@ -504,8 +504,6 @@ Statistics::init(const Level& level, const Statistics::Preferences& preferences)
   m_total_coins = level.get_total_coins();
   m_total_badguys = level.get_total_badguys();
   m_total_secrets = level.get_total_secrets();
-
-  m_preferences = preferences;
 }
 
 void

src/supertux/statistics.hpp
@@ -78,6 +78,7 @@ public:
   /** unserialize statistics object from squirrel table "statistics" */
   void unserialize_from_squirrel(const ssq::Table& table);
 
+  inline Preferences& get_preferences() { return m_preferences; }
   inline const Preferences& get_preferences() const { return m_preferences; }
   void add_preferences_to_menu(Menu& menu);
 
@@ -88,7 +89,7 @@ public:
   /** Updates the timers for in-game stats rendering. Should be used from the same object that calls draw_ingame_stats(). */
   void update_timers(float dt_sec);
 
-  void init(const Level& level, const Preferences& preferences);
+  void init(const Level& level);
   void finish(float time);
   void invalidate();
 

src/worldmap/level_tile.cpp
@@ -71,6 +71,8 @@ LevelTile::LevelTile(const ReaderMapping& mapping) :
 
   if (in_worldmap())
     load_level_information();
+
+  set_action("default");
 }
 
 LevelTile::~LevelTile()
@@ -108,15 +110,19 @@ LevelTile::load_level_information()
 
       mapping.get("name", m_title);
       mapping.get("target-time", m_target_time);
+
+      std::optional<ReaderMapping> level_stat_preferences;
+      if (mapping.get("statistics", level_stat_preferences))
+        m_statistics.get_preferences().parse(*level_stat_preferences);
     }
-    catch (std::exception& err)
+    catch (const std::exception& err)
     {
       std::stringstream out;
       out << "Cannot read level info: " << err.what() << std::endl;
       throw std::runtime_error(out.str());
     }
   }
-  catch (std::exception& err)
+  catch (const std::exception& err)
   {
     log_warning << "Problem when reading level information: " << err.what() << std::endl;
     return;